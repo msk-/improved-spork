2f28229247dd91415994600cc8670c91c182189e
Matt Kingston <matt.kingston+git@gmail.com@>
2018-05-03 18:19:09 +0100

    Added a little extra testing code.

--- src/Lib.hs
+++ src/Lib.hs
@@ -2,14 +2,22 @@ module Lib
     ( someFunc
     ) where
 
-import Control.Lens ((&), (.~), (?~), Traversal', (%~), each)
-import Data.Swagger (Swagger)
-import Data.Swagger.Lens (paths, operationId)
+import Control.Lens ((&), (.~), (?~), (%~))
+import Data.Swagger (Swagger, Tag)
+import Data.Swagger.Lens (paths, operationId, info, description, tags)
 import qualified Data.HashMap.Strict.InsOrd as InsOrdHashMap
 import Data.Text (Text(..), pack)
+import Data.Set (fromList)
+import Data.Monoid
 
 genOpIds :: Swagger -> Swagger
 genOpIds = paths %~ InsOrdHashMap.mapWithKey (\k v -> v & operationId ?~ (pack "hello"))
+
+writeInfoTitle :: Swagger -> Swagger
+writeInfoTitle = info.description ?~ (pack "whatever description")
+
+writeTags :: [Tag] -> Swagger -> Swagger
+writeTags ts = tags %~ (<> fromList ts)
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
